a3e3ddf1c615cda321ed2f69ba585c04787b9801
Philipp Mössl <pmoessl@outlook.de>
2023-08-11 11:40:07 +0200

    add a delete function for new created categorys

--- templates/js/addTask.js
+++ templates/js/addTask.js
@@ -277,11 +277,11 @@ function clearFields() {
         document.getElementById(`renderVisibelAssigned${i}`).classList.remove('d-none');
         document.getElementById(`assignedCheckbox${i}`).checked = false;
     }
-        assignedToNames = [];
-        allSubtasks = [];
-        document.getElementById('assignedToList').innerHTML = '';
-        document.getElementById('subtasksList').innerHTML = '';
-    }
+    assignedToNames = [];
+    allSubtasks = [];
+    document.getElementById('assignedToList').innerHTML = '';
+    document.getElementById('subtasksList').innerHTML = '';
+}
 
 /**
  * 
@@ -364,6 +364,12 @@ function showAddBox() {
     } else {
         document.getElementById('new-category-box').classList.add('d-none');
     }
+    //check if a new category is choosen
+    if (newCategorys.includes(value)) {
+        document.getElementById('delete-category-btn').classList.remove('d-none');
+    } else {
+        document.getElementById('delete-category-btn').classList.add('d-none');
+    }
 }
 
 /**
@@ -403,20 +409,26 @@ function addNewCategory() {
  */
 function setNewCategory(categoryInput) {
     let selection = document.getElementById('category');
-    selection.innerHTML += `<option value="${categoryInput}">${categoryInput}</option>`;
+    selection.innerHTML += `
+    <option class="delete-category" value="${categoryInput}">${categoryInput}
+
+    </option>`;
     newCategorys.push(categoryInput);
     localStorage.setItem('newCategorys', JSON.stringify(newCategorys));
+    selection.value = categoryInput;
+    document.getElementById('new-category-box').classList.add('d-none');
+    document.getElementById('delete-category-btn').classList.remove('d-none');
 }
 
 /**
  * 
- * render THe new Category
+ * render the new Category
  */
 function renderNewCategorys() {
     let selection = document.getElementById('category');
     let parsedCategorys = JSON.parse(localStorage.getItem('newCategorys'));
-
-
+    selection.innerHTML = '';
+    renderStandardOptions(selection);
     if (parsedCategorys) {
         newCategorys = parsedCategorys;
         for (let i = 0; i < newCategorys.length; i++) {
@@ -424,4 +436,42 @@ function renderNewCategorys() {
             selection.innerHTML += `<option value="${category}">${category}</option>`;
         }
     }
+}
+
+/**
+ * render the standard options in the selection field
+ * 
+ * @param {html element} selection selection field getting by id
+ */
+function renderStandardOptions(selection) {
+    selection.innerHTML += `
+    <option value="" disabled selected>Select task category</option>
+    <option value="new">Create new category</option>
+    <option value="design">Design</option>
+    <option value="sales">Sales</option>
+    <option value="backoffice">Backoffice</option>
+    <option value="media">Media</option>
+    <option value="marketing">Marketing</option>
+    `;
+}
+
+/**
+ * delete the new created categorys
+ * 
+ */
+function deleteCategory() {
+    let currentCategory = document.getElementById('category').value;
+    let position = -1;
+    //check which position in array the category is 
+    for (let i = 0; i < newCategorys.length; i++) {
+        const place = newCategorys[i];
+        if (currentCategory == place) {
+            position = place;
+            newCategorys.splice(position, 1);
+            localStorage.setItem('newCategorys', JSON.stringify(newCategorys));
+            renderNewCategorys();
+        }
+    }
+    document.getElementById('delete-category-btn').classList.add('d-none');
+    
 }
